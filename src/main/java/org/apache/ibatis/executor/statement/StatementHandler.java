10a3607339ae59d0bd407f2e05730b53e713e464
chenxing <1057791979@qq.com>
2024-04-25 21:03:44 +0800

    用于处理Statement的StatementHandler

--- src/main/java/org/apache/ibatis/executor/statement/StatementHandler.java
+++ src/main/java/org/apache/ibatis/executor/statement/StatementHandler.java
@@ -1,55 +1,79 @@
 /**
- *    Copyright 2009-2016 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2016 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.executor.statement;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
 
 import org.apache.ibatis.cursor.Cursor;
 import org.apache.ibatis.executor.parameter.ParameterHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.session.ResultHandler;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
 /**
  * @author Clinton Begin
+ * 定义了SQL执行的操作
  */
 public interface StatementHandler {
 
-  Statement prepare(Connection connection, Integer transactionTimeout)
-      throws SQLException;
+    /**
+     * 获取Statement对象
+     *
+     * @param connection         连接
+     * @param transactionTimeout 事务超时时间
+     */
+    Statement prepare(Connection connection, Integer transactionTimeout)
+            throws SQLException;
 
-  void parameterize(Statement statement)
-      throws SQLException;
+    /**
+     * 为SQL语句绑定实参
+     *
+     * @param statement statement对象
+     */
+    void parameterize(Statement statement)
+            throws SQLException;
 
-  void batch(Statement statement)
-      throws SQLException;
+    /**
+     * 批量执行
+     */
+    void batch(Statement statement)
+            throws SQLException;
 
-  int update(Statement statement)
-      throws SQLException;
+    /**
+     * 单条更新
+     */
+    int update(Statement statement)
+            throws SQLException;
 
-  <E> List<E> query(Statement statement, ResultHandler resultHandler)
-      throws SQLException;
+    /**
+     * 单条查询
+     */
+    <E> List<E> query(Statement statement, ResultHandler resultHandler)
+            throws SQLException;
 
-  <E> Cursor<E> queryCursor(Statement statement)
-      throws SQLException;
+    /**
+     * 游标查询，用于超大结果集
+     */
+    <E> Cursor<E> queryCursor(Statement statement)
+            throws SQLException;
 
-  BoundSql getBoundSql();
+    BoundSql getBoundSql();
 
-  ParameterHandler getParameterHandler();
+    ParameterHandler getParameterHandler();
 
 }
