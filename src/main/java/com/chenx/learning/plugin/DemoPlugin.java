8088257038046cb88efb71acb1b0b48cc4636644
chenxing <1057791979@qq.com>
2024-06-24 19:27:21 +0800

    plugin包，提供了Plugin类处理拦截器。Interceptor接口自定义拦截器，以及定义拦截器需要的注解。

--- src/main/java/com/chenx/learning/plugin/DemoPlugin.java
+++ src/main/java/com/chenx/learning/plugin/DemoPlugin.java
@@ -10,8 +10,10 @@ import org.apache.ibatis.plugin.Signature;
 
 import java.lang.reflect.Field;
 import java.sql.Connection;
+import java.util.Properties;
 
 // 拦截器需要添加到mybatis-config.xml中来完成注册
+// 每个@Signature都声明了当前拦截器要拦截的方法，表示拦截type类型中的method方法，参数类型列表是args，多个@Signature方法表示拦截多个方法
 @Intercepts({
         // @Signature 注解用来指定 DemoPlugin 插件实现类要拦截的目标方法信息，可以有多个
 //        @Signature(type = Executor.class, method = "query", args = {
@@ -24,6 +26,7 @@ public class DemoPlugin implements Interceptor {
     private int logLevel;
 
     /**
+     * 被拦截的方法会转到来执行这个方法
      * 给sql加上limit 1, 1子句
      *
      * @param invocation
@@ -41,6 +44,17 @@ public class DemoPlugin implements Interceptor {
 //        sql.set(boundSql, originSql + " limit 1, 1");
         System.out.println(boundSql.getSql());
         System.out.println("查询开始执行");
+        // 执行原有方法并返回。
         return invocation.proceed();
+    }
+
+    /**
+     * 为拦截器设置属性
+     *
+     * @param properties 配置属性
+     */
+    @Override
+    public void setProperties(Properties properties) {
+        Interceptor.super.setProperties(properties);
     }
 }
--- src/main/java/org/apache/ibatis/plugin/Plugin.java
+++ src/main/java/org/apache/ibatis/plugin/Plugin.java
@@ -27,11 +27,15 @@ import java.util.Set;
 
 /**
  * @author Clinton Begin
+ * 继承InvocationHandler，实现基于反射的代理类。
  */
 public class Plugin implements InvocationHandler {
 
+    // 被代理对象
     private final Object target;
+    // 拦截器
     private final Interceptor interceptor;
+    // 拦截器要拦截的所有类，以及类中对应的方法
     private final Map<Class<?>, Set<Method>> signatureMap;
 
     private Plugin(Object target, Interceptor interceptor, Map<Class<?>, Set<Method>> signatureMap) {
@@ -41,16 +45,22 @@ public class Plugin implements InvocationHandler {
     }
 
     /**
-     * 用于给拦截器插件代理对象
+     * 如果一个类将会被拦截器拦截，会通过此方法为该类从创建一个代理对象。否则不做任何操作
+     *
+     * @param target      被代理对象
+     * @param interceptor 拦截器
+     * @return 用来替换被代理对象的代理对象
      */
     public static Object wrap(Object target, Interceptor interceptor) {
-        // 获取自定义Interceptor实现欸上的@Signature注解信息
+        // 获取自定义Interceptor实现类上的@Signature注解信息，得到要拦截的所有类型和具体方法
         Map<Class<?>, Set<Method>> signatureMap = getSignatureMap(interceptor);
+        // 获取被代理对象的类型
         Class<?> type = target.getClass();
-        // 检查当前传入的target对下个你是否为@Signature要拦截的类型
-        // 如果是，使用JDK动态代理创建代理对象
+        // 逐级寻找被代理对象类型的父类。将父类中需要被拦截的也找出来
         Class<?>[] interfaces = getAllInterfaces(type, signatureMap);
+        // 只要父类中有一个是需要被拦截的，说明被代理对象是需要被拦截的
         if (interfaces.length > 0) {
+            // 根据本类，创建代理对象并返回。
             return Proxy.newProxyInstance(
                     type.getClassLoader(),
                     interfaces,
@@ -59,30 +69,43 @@ public class Plugin implements InvocationHandler {
         return target;
     }
 
+    /**
+     * 需要被拦截的方法的对象会被wrap()方法替换成代理对象，后面执行方法就会进入本方法
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         try {
-            // 获取当前待执行方法所属的类
+            // 获取当前对象所属类所有需要拦截的方法。
             Set<Method> methods = signatureMap.get(method.getDeclaringClass());
-            // 如果当前方法需要被代理，执行intercept()方法进行拦截处理
+            // 如果当前方法需要被拦截器处理，执行intercept()方法进行拦截处理
             // intercept()方法就是插件自定义的处理逻辑
             if (methods != null && methods.contains(method)) {
                 return interceptor.intercept(new Invocation(target, method, args));
             }
-            // 如果当前方法不需要被代理，则调用target对象的相应方法
+            // 如果当前方法不需要被代理，则调用target对象执行原来的逻辑
             return method.invoke(target, args);
         } catch (Exception e) {
             throw ExceptionUtil.unwrapThrowable(e);
         }
     }
 
+    /**
+     * 获取拦截器要拦截的所有类和其中的要拦截的方法
+     *
+     * @param interceptor 拦截器
+     * @return 参数中的拦截器要拦截的所有类的所有要被拦截的方法
+     */
     private static Map<Class<?>, Set<Method>> getSignatureMap(Interceptor interceptor) {
+        // 获取@Intercepts注解
         Intercepts interceptsAnnotation = interceptor.getClass().getAnnotation(Intercepts.class);
         // issue #251
         if (interceptsAnnotation == null) {
             throw new PluginException("No @Intercepts annotation was found in interceptor " + interceptor.getClass().getName());
         }
+
+        // 取出@Intercepts注解的value属性，是一个Signature数组
         Signature[] sigs = interceptsAnnotation.value();
+        // 将Signature分解为一个Map，键是其type属性，值是method属性 + args属性对应的Method类
         Map<Class<?>, Set<Method>> signatureMap = new HashMap<>();
         for (Signature sig : sigs) {
             Set<Method> methods = signatureMap.computeIfAbsent(sig.type(), k -> new HashSet<>());
